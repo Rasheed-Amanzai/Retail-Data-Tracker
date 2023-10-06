dda1f1e36f76c54c96977e0dd0c5e7a9995cae35
Rasheed Amanzai <amanzair@sheridancollege.ca>
2023-10-05 20:04:28 -0400

    Add project files.

---
+++ Retail Data Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Retail_Data_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
