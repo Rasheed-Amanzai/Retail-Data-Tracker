3eddb12cb64c8a77907b6a01c74c02f3bc0af385
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-09 02:24:26 -0500

    Added Items controller and views

--- Retail Data Tracker/Models/Item.cs
+++ Retail Data Tracker/Models/Item.cs
@@ -7,14 +7,25 @@ namespace Retail_Data_Tracker.Models
 {
     public class Item
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string ItemDesc { get; set; }
         public int Quantity { get; set; }
-        public string Supplier { get; set; }
+        public int SupplierId { get; set; }
 
         public double BuyCost { get; set;}
         public double SellCost {get; set;}
         public bool IsChecked { get; set; }
+        public Supplier ItemSupplier { get; set; }
 
+        public Supplier GetSupplier()
+        {
+            return ItemSupplier;
+        }
+
+        public void SetSupplier(Supplier supplier)
+        {
+            ItemSupplier = SupplierRepository.Suppliers.FirstOrDefault(s => s.Id == SupplierId);
+        }
     }
 }
