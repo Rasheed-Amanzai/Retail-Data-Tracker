e01e50418a1da6700d21ee281faca113d0b20c20
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-09 02:25:23 -0500

    Added proper context for operations

---
+++ Retail Data Tracker/Data/Retail_Data_TrackerContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Retail_Data_Tracker.Models;
+
+namespace Retail_Data_Tracker.Data
+{
+    public class Retail_Data_TrackerContext : DbContext
+    {
+        public Retail_Data_TrackerContext (DbContextOptions<Retail_Data_TrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Retail_Data_Tracker.Models.Item> Items { get; set; } = default!;
+        public DbSet<Retail_Data_Tracker.Models.Supplier> Suppliers { get; set; } = default!;
+        public DbSet<Retail_Data_Tracker.Models.Order> Orders { get; set; } = default!;
+
+    }
+}
