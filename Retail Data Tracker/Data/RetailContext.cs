e00b2a91dbd062c2e91c7e0a6eddeb0b7ace9b8d
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-09 02:25:34 -0500

    Update RetailContext.cs

--- Retail Data Tracker/Data/RetailContext.cs
+++ Retail Data Tracker/Data/RetailContext.cs
@@ -4,10 +4,16 @@ using Microsoft.EntityFrameworkCore;
 namespace Retail_Data_Tracker.Data
 {
     public class RetailContext : DbContext
-    {
-        public RetailContext(DbContextOptions<RetailContext> options) : base(options) { }
+    { 
+        public RetailContext(DbContextOptions<RetailContext> options) : base(options)
+        {
+        }
 
         public DbSet<Supplier> Suppliers { get; set; }
         // Add DbSets for other entity/model classes (Item, Order, etc.)
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
