ad48e1cd6234a2db0df9ddc03c2cd59e98dc9ac3
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-11 15:02:11 -0500

    New migration

---
+++ Retail Data Tracker/Migrations/20231211200124_Mig3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Retail_Data_Tracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quantity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuantityNumber = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quantity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quantity_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quantity_OrderId",
+                table: "Quantity",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quantity");
+        }
+    }
+}
