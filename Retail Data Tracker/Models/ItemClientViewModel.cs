b02729e518fee7ac0803bf4353587226a6deff75
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-12 00:38:55 -0500

    Item Views for Order

---
+++ Retail Data Tracker/Models/ItemClientViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Retail_Data_Tracker.Models
+{
+    public class ItemClientViewModel
+    {
+        public List<Item> Items { get; set; }
+        public List<Client> Clients { get; set; }
+        public int SelectedClientId { get; set; }
+    }
+}
---
+++ Retail Data Tracker/Models/ItemQuantityViewModel.cs
@@ -0,0 +1,7 @@
+﻿using Retail_Data_Tracker.Models;
+
+public class ItemQuantityViewModel
+{
+    public Item Item { get; set; }
+    public int Quantity { get; set; }
+}
