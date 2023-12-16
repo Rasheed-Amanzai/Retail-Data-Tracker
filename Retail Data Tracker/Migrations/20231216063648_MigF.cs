adb723c438f62737de7952184694cf63cb326ac6
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-16 02:17:54 -0500

    Many-to-many for Items and Orders

---
+++ Retail Data Tracker/Migrations/20231216063648_MigF.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Retail_Data_Tracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigF : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Quantity");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_OrderId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "OrderItem",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    QuantityNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItem", x => new { x.OrderId, x.ItemId });
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ItemId",
+                table: "OrderItem",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Quantity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: true),
+                    QuantityNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quantity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quantity_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_OrderId",
+                table: "Items",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quantity_OrderId",
+                table: "Quantity",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+    }
+}
