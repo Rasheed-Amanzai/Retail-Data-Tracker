9bf87c494ea02a8f08660af5f86d3982db5a1db9
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-16 12:47:09 -0500

    Item Create Viewmodel Initialized

---
+++ Retail Data Tracker/Models/ItemCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Retail_Data_Tracker.Models
+{
+
+    public class ItemCreateViewModel
+    {
+        public string Name { get; set; }
+        public string ItemDesc { get; set; }
+        public int Quantity { get; set; }
+        public int SupplierId { get; set; }
+        public double BuyCost { get; set; }
+        public double SellCost { get; set; }
+
+
+        // Property to hold the list of suppliers
+        public List<Supplier> Suppliers { get; set; }
+    }
+}
