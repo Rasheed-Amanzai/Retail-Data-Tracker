ecb3271f72b7298aa8239968e678872a41202f14
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-16 10:38:50 -0500

    Added supplierlist for viewmodel

--- Retail Data Tracker/Models/ItemClientViewModel.cs
+++ Retail Data Tracker/Models/ItemClientViewModel.cs
@@ -4,6 +4,7 @@
     {
         public List<Item> Items { get; set; }
         public List<Client> Clients { get; set; }
+        public List<Supplier> Suppliers { get; set; } = new List<Supplier>();
         public int SelectedClientId { get; set; }
     }
 }
