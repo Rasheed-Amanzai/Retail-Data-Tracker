d9ce893f24acf04905442ad54b6ea84cc1ffc0de
Mat N <97564308+afk-m@users.noreply.github.com>
2023-12-16 12:46:57 -0500

    Constraints

--- Retail Data Tracker/Models/Item.cs
+++ Retail Data Tracker/Models/Item.cs
@@ -1,28 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 
 namespace Retail_Data_Tracker.Models
 {
     public class Item
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 255 characters")]
         public string Name { get; set; }
+        
+        [StringLength(1000, ErrorMessage = "Item description cannot exceed 1000 characters")]
         public string ItemDesc { get; set; }
+        
         public int Quantity { get; set; }
         public int QuantityWanted { get; set; }
         public int SupplierId { get; set; }
-
+        
+        [Required(ErrorMessage = "Buy cost is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Buy cost must be greater than 0")]
         public double BuyCost { get; set;}
+
+        [Required(ErrorMessage = "Sell cost is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Sell cost must be greater than 0")]
         public double SellCost {get; set;}
         public bool IsChecked { get; set; }
-        public Supplier ItemSupplier { get
-            {
-                var itemSupplier = SupplierRepository.Suppliers.FirstOrDefault(s => s.Id == SupplierId);
-                return itemSupplier;
-            }
-        }
+        [BindNever]
+        public Supplier ItemSupplier { get; set; }
 
         public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
